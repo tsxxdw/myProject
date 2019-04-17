5edba2004e383962ff562bfd291018be221b40fa
duansj <494456995@qq.com>
2019-04-17 14:30:47 +0800

    lock

--- javademo/src/main/java/lock/demo1/Doctor.java
+++ javademo/src/main/java/lock/demo1/Doctor.java
@@ -2,8 +2,8 @@ package lock.demo1;
 
 public class Doctor {
     //治理的方法
-    public synchronized void cures(String a){
-            System.out.println("我正在治理:"+a);
-        System.out.println(a+"治理结束");
+    public  void cures(String a){
+            System.out.println("我正在治疗:"+a);
+
     }
 }
--- javademo/src/main/java/lock/demo2/Child.java
+++ javademo/src/main/java/lock/demo2/Child.java
@@ -1,23 +1,24 @@
 package lock.demo2;
 
 public class Child extends Thread {
+    @Override
     public void run() {
         while (true) {
             synchronized (Kuang.kuang) {
-//1.筐里没水果了就让小孩休息
+                 //1.筐里没水果了就让小孩休息
                 if (Kuang.kuang.size() == 0) {
                     try {
                         Kuang.kuang.wait();
                     } catch (InterruptedException e) {
                     }
                 }
-//2.小孩吃水果
+                //2.小孩吃水果
                 Kuang.kuang.remove("apple");
                 System.out.println("小孩吃了一个水果,目前筐里有" + Kuang.kuang.size() + "个水果");
-//3.唤醒农夫继续放水果
+                //3.唤醒农夫继续放水果
                 Kuang.kuang.notify();
             }
-//4.模拟控制速度try {
+             //4.模拟控制速度try {
             try {
                 Thread.sleep(400);
             } catch (InterruptedException e) {

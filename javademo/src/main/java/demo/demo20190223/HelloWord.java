a4c42bc0d0ea47af9c06033b36b0486ba139f7ee
duansj <494456995@qq.com>
2019-04-19 10:07:14 +0800

    noinfo

--- javademo/src/main/java/demo/demo20190223/HelloWord.java
+++ javademo/src/main/java/demo/demo20190223/HelloWord.java
@@ -3,7 +3,10 @@ package demo.demo20190223;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.printf("hello world");
+        String o="aaa-bbb";
+        System.out.println(o.replace(o.substring(0,o.indexOf("-")+1), ""));;
+        System.out.println(o.replace(o.substring(o.indexOf("-"),o.length()), ""));;
+        System.out.println("hello world");
     }
 }
 

3de0b176286a63a23f35a6c35f1a44d6979efea3
duansj <494456995@qq.com>
2019-04-20 17:42:52 +0800

    HashMap 95%

---
+++ javademo/src/main/java/demo/demo/Demo.java
@@ -0,0 +1,14 @@
+package demo.demo;
+
+/**
+ * @Author created by dsj
+ * @Date 2019/4/20 16:18
+ * @Description ...
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+        int j2 = 129&4;
+        System.out.println(j2);
+    }
+}

f0b01781b584cd59924d480d37ba644fa739836c
tsxxdw <494456995@qq.com>
2019-08-14 17:29:50 +0800

    水印升级

--- myUtils/src/main/java/cn/tsxxdw/service/mypic/MyPicUtils.java
+++ myUtils/src/main/java/cn/tsxxdw/service/mypic/MyPicUtils.java
@@ -1,20 +1,20 @@
 package cn.tsxxdw.service.mypic;
 
 import cn.tsxxdw.mybean.vo.ResultVo;
-import cn.tsxxdw.service.mylog.MyLogUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Objects;
-
 @Slf4j
 public class MyPicUtils {
     public static ResultVo compressPic(String source, String target) {
@@ -101,5 +101,125 @@ public class MyPicUtils {
         return new ResultVo().setSuccess(suffix);
     }
 
+
+    /**
+     * 制作水印
+     */
+    //  markImageByText("hello world","D:/1.jpg","D:2.jpg",45,new Color(0,0,0),"JPG");
+    public static ResultVo<Void> watermark_1(String centerLogoText, String source,String target,Integer degree,Color color,String picSuffix){
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            // 1、源图片
+            java.awt.Image srcImg = ImageIO.read(new File(source));
+            BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null),srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            // 2、得到画笔对象
+            Graphics2D g = buffImg.createGraphics();
+            // 3、设置对线段的锯齿状边缘处理
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), java.awt.Image.SCALE_SMOOTH), 0, 0, null);
+            // 4、设置水印旋转
+            if (null != degree) {
+                g.rotate(Math.toRadians(degree),  buffImg.getWidth()/2,buffImg.getHeight() /2);
+            }
+            // 5、设置水印文字颜色
+            g.setColor(color);
+            // 6、设置水印文字Font
+            g.setFont(new java.awt.Font("宋体", java.awt.Font.BOLD, buffImg.getHeight() /2));
+            // 7、设置水印文字透明度
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 0.15f));
+            // 8、第一参数->设置的内容，后面两个参数->文字在图片上的坐标位置(x,y)
+            g.drawString(centerLogoText,  buffImg.getWidth()/2 , buffImg.getHeight()/2);
+            // 9、释放资源
+            g.dispose();
+            // 10、生成图片
+            os = new FileOutputStream(target);
+            ImageIO.write(buffImg, picSuffix, os);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != is)
+                    is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return ResultVo.createSimpleSuccessResult();
+    }
+
+    public static ResultVo<Void> watermark_2(String centerLogoText,String rightAndrigthLogoText, String source,String target,Color color,String picSuffix){
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            // 1、源图片
+            java.awt.Image srcImg = ImageIO.read(new File(source));
+            BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null),srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            // 2、得到画笔对象
+            Graphics2D g = buffImg.createGraphics();
+            // 3、设置对线段的锯齿状边缘处理
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), java.awt.Image.SCALE_SMOOTH), 0, 0, null);
+            // 4、设置水印旋转
+
+             //   g.rotate(Math.toRadians(degree),  buffImg.getWidth()/2,buffImg.getHeight() /2);
+
+            // 5、设置水印文字颜色
+            g.setColor(color);
+
+
+            g.rotate(Math.toRadians(0),  0,0 );
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 1));
+            g.setFont(new java.awt.Font("宋体", Font.LAYOUT_NO_START_CONTEXT, buffImg.getHeight() /30));
+            g.drawString(rightAndrigthLogoText,  50 , buffImg.getHeight()-50);
+
+
+
+
+
+            g.setFont(new java.awt.Font("宋体", java.awt.Font.BOLD, buffImg.getHeight() /10));
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 0.15f));
+            g.rotate(Math.toRadians(45),  buffImg.getWidth()/2,buffImg.getHeight() /2);
+            g.drawString(centerLogoText,  buffImg.getWidth()/3 , buffImg.getHeight()/2);
+
+
+
+
+            // 9、释放资源
+            g.dispose();
+            // 10、生成图片
+            os = new FileOutputStream(target);
+            ImageIO.write(buffImg, picSuffix, os);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != is)
+                    is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return ResultVo.createSimpleSuccessResult();
+    }
+
+    public static void main(String[] args) {
+        String source="D:/data/4_uploadfile/images/vegetable_chart/pic/2019/08/14/20190814161801-35ff52d548.JPG";
+        String target="D:/data/4_uploadfile/images/vegetable_chart/pic/2019/08/14/20190814161801-35ff52d548_target2.JPG";
+        MyPicUtils.watermark_2("美菜图","图片来源：外卖名堂美菜图",source,target,new Color(0,0,0),"JPG");
+
+    }
 
 }

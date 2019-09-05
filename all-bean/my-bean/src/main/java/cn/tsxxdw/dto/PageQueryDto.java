dd2a2cb913fda50c5b9a01ca2c8514754bb50f90
tsxxdw <494456995@qq.com>
2019-09-05 16:42:57 +0800

    no info

--- all-bean/my-bean/src/main/java/cn/tsxxdw/dto/PageQueryDto.java
+++ all-bean/my-bean/src/main/java/cn/tsxxdw/dto/PageQueryDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class PageQueryDto {
-    private Integer current;
-    private Integer size;
+    private Integer page;
+    private Integer limit;
+    private Integer count;//总条数
 }

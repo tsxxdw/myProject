ee763623215c4c16972c666331f84c37a447b143
tsxxdw <494456995@qq.com>
2019-09-06 18:16:50 +0800

    公共的分页方法完成

--- myBase/src/main/java/cn/tsxxdw/mybese/BaseService.java
+++ myBase/src/main/java/cn/tsxxdw/mybese/BaseService.java
@@ -60,6 +60,8 @@ public class BaseService<E, M extends BaseMapper<E>> {
     public List<E> selectPage(Object object, NullSafeWrapper where) throws Exception {
         Map<String, String> fieldMap = new HashMap<>();
         Field[] field = object.getClass().getDeclaredFields(); //获取实体类的所有属性，返回Field数组
+        Integer page = null;
+        Integer limit = null;
         for (int j = 0; j < field.length; j++) { //遍历所有属性
             String camelFiledName = field[j].getName(); //获取属性的名字
             String name = camelFiledName.substring(0, 1).toUpperCase() + camelFiledName.substring(1); //将属性的首字符大写，方便构造get，set方法
@@ -69,10 +71,16 @@ public class BaseService<E, M extends BaseMapper<E>> {
             String value = (String) m.invoke(object); //调用getter方法获取属性值
             String underlineFiledName = MyStrUtils.camelToUnderline(camelFiledName, 1);
             if (StringUtils.isBlank(value)) continue;
-            where.eq(underlineFiledName,value);
+            if ("limit".equals(camelFiledName)) {
+                 limit=Integer.parseInt(value);
+            }
+            if ("page".equals(camelFiledName)) {
+                 page=Integer.parseInt(value);
+            }
+            where.eq(underlineFiledName, value);
         }
-        Page<E> page = null;
-        IPage<E> iPage = getM().selectPage(page, where);
+        Page<E> myPage = new Page<>(page, limit);
+        IPage<E> iPage = getM().selectPage(myPage, where);
         List<E> list = iPage.getRecords();
         return list;
     }

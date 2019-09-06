2491d53f6ef47aef0a62cbcfd1fedd79d0227158
tsxxdw <494456995@qq.com>
2019-09-06 18:06:35 +0800

    公共的分页方法

--- myBase/src/main/java/cn/tsxxdw/mybese/BaseService.java
+++ myBase/src/main/java/cn/tsxxdw/mybese/BaseService.java
@@ -1,12 +1,21 @@
 package cn.tsxxdw.mybese;
 
 
+import cn.tsxxdw.other.NullSafeWrapper;
+import cn.tsxxdw.service.mystr.MyStrUtils;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -45,6 +54,26 @@ public class BaseService<E, M extends BaseMapper<E>> {
 
     public List<E> selectList(Wrapper wrapper) {
         List<E> list = m.selectList(wrapper);
+        return list;
+    }
+
+    public List<E> selectPage(Object object, NullSafeWrapper where) throws Exception {
+        Map<String, String> fieldMap = new HashMap<>();
+        Field[] field = object.getClass().getDeclaredFields(); //获取实体类的所有属性，返回Field数组
+        for (int j = 0; j < field.length; j++) { //遍历所有属性
+            String camelFiledName = field[j].getName(); //获取属性的名字
+            String name = camelFiledName.substring(0, 1).toUpperCase() + camelFiledName.substring(1); //将属性的首字符大写，方便构造get，set方法
+            String type = field[j].getGenericType().toString(); //获取属性的类型
+            //如果type是类类型，则前面包含"class "，后面跟类名
+            Method m = object.getClass().getMethod("get" + name);
+            String value = (String) m.invoke(object); //调用getter方法获取属性值
+            String underlineFiledName = MyStrUtils.camelToUnderline(camelFiledName, 1);
+            if (StringUtils.isBlank(value)) continue;
+            where.eq(underlineFiledName,value);
+        }
+        Page<E> page = null;
+        IPage<E> iPage = getM().selectPage(page, where);
+        List<E> list = iPage.getRecords();
         return list;
     }
 
--- myUtils/src/main/java/cn/tsxxdw/service/mystr/MyStrUtils.java
+++ myUtils/src/main/java/cn/tsxxdw/service/mystr/MyStrUtils.java
@@ -1,11 +1,16 @@
 package cn.tsxxdw.service.mystr;
 
 import cn.tsxxdw.service.mydate.MyDateUtil;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.DateUtil;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class MyStrUtils {
+    public static final char UNDERLINE = '_';
     /**
      *
      * @return String 获取一个字符，日期+随即字符串
@@ -16,5 +21,55 @@ public class MyStrUtils {
         sb.append("-");
         sb.append( UUID.randomUUID().toString().replaceAll("-", "").substring(0,10));
         return  sb.toString();
+    }
+
+
+    //驼峰转下划线
+    public static String camelToUnderline(String param, Integer charType) {
+        if (param == null || "".equals(param.trim())) {
+            return "";
+        }
+        int len = param.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = param.charAt(i);
+            if (Character.isUpperCase(c)) {
+                sb.append(UNDERLINE);
+            }
+            if (charType == 2) {
+                sb.append(Character.toUpperCase(c));  //统一都转大写
+            } else {
+                sb.append(Character.toLowerCase(c));  //统一都转小写
+            }
+
+
+        }
+        return sb.toString();
+    }
+
+    //下划线转驼峰
+    public static String underlineToCamel(String param) {
+        if (param == null || "".equals(param.trim())) {
+            return "";
+        }
+        int len = param.length();
+        StringBuilder sb = new StringBuilder(len);
+        Boolean flag = false; // "_" 后转大写标志,默认字符前面没有"_"
+        for (int i = 0; i < len; i++) {
+            char c = param.charAt(i);
+            if (c == UNDERLINE) {
+                flag = true;
+                continue;   //标志设置为true,跳过
+            } else {
+                if (flag == true) {
+                    //表示当前字符前面是"_" ,当前字符转大写
+                    sb.append(Character.toUpperCase(param.charAt(i)));
+                    flag = false;  //重置标识
+                } else {
+                    sb.append(Character.toLowerCase(param.charAt(i)));
+                }
+            }
+        }
+        return sb.toString();
     }
 }

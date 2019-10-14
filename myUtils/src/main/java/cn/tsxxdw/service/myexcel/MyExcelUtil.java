8adf93c3e9b1400caef9f3742bb233945741fca8
tsxxdw <494456995@qq.com>
2019-10-14 15:40:45 +0800

    excel 更新等级

--- myUtils/src/main/java/cn/tsxxdw/service/myexcel/MyExcelUtil.java
+++ myUtils/src/main/java/cn/tsxxdw/service/myexcel/MyExcelUtil.java
@@ -1,8 +1,11 @@
 package cn.tsxxdw.service.myexcel;
 
 import cn.tsxxdw.bean.vo.excel.ExcelReadVo;
+import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.metadata.Sheet;
+
+import java.util.List;
 
 /**
  * @Author created by dsj
@@ -12,5 +15,14 @@ import com.alibaba.excel.metadata.Sheet;
 public class MyExcelUtil {
     public static void read(ExcelReadVo excelReadVo) {
         EasyExcelFactory.readBySax(excelReadVo.getInputStream(), new Sheet(excelReadVo.getSheetNo(), excelReadVo.getHeadLineMun()), new MyExcelListener(excelReadVo));
+    }
+
+
+    public static void write(String fileName,Class myclass, List list){
+        // 写法1
+        //String fileName = "/data/4_uploadfile/pachong/20191014_sz" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去读，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, myclass).sheet("模板").doWrite(list);
     }
 }

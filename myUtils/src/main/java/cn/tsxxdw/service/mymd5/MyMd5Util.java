2f2e47fffd4788b95597d59453b45effbe90e6f5
tsxxdw <494456995@qq.com>
2019-11-03 10:35:55 +0800

    md5 加密

---
+++ myUtils/src/main/java/cn/tsxxdw/service/mymd5/MyMd5Util.java
@@ -0,0 +1,36 @@
+package cn.tsxxdw.service.mymd5;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MyMd5Util {
+    public static String getMD5Str(String str) {
+        MessageDigest messageDigest = null;
+        try {
+            try {
+                messageDigest = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            messageDigest.reset();
+            messageDigest.update(str.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        byte[] byteArray = messageDigest.digest();
+
+        StringBuffer md5StrBuff = new StringBuffer();
+
+        for (int i = 0; i < byteArray.length; i++) {
+            if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)
+                md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+            else
+                md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+        }
+        return md5StrBuff.toString();
+    }
+
+}
